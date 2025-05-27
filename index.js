9b0eeed2dbb857de08e5a78aa5e729e57a9c9ffb
Tanisha patra <tanishapatra88@gmail.com>
2025-05-27 11:41:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Set view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
